433010823a45ca5d8d041a0b7422bff59c4a8794
Shmuel <sm0524432584@gmail.com>
2026-06-24 10:57:16 +0300

    create frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client">
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: { input: { main: path.resolve(__dirname, 'index.html') } },
+    cssCodeSplit: true,
+    target: 'modules',
+  },
+  define: {
+    'import.meta.env': process.env,
+  },
+  server: {
+    host: true,
+    port: 3000,
+    watch: { usePolling: true },
+  },
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
+    exclude: ['react-toastify'],
+  },
+});
